a984bf06e4707f54ed97ab880d57fa2f1eb00d8c
Lam Khanh Duy <23110084@student.hcmute.edu.vn>
2025-12-06 23:24:58 +0700

    Update

--- quanlytour/routes/bookingRoutes.js
+++ quanlytour/routes/bookingRoutes.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import * as bookingController from '../controllers/bookingController.js';
+import { protect, adminOnly } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-// CRUD routes
-router.post('/', bookingController.createBooking); // Tạo booking mới
-router.get('/', bookingController.getBookings); // Lấy danh sách booking
-router.get('/:id', bookingController.getBookingById); // Lấy booking theo ID
-router.put('/:id', bookingController.updateBooking); // Cập nhật booking theo ID
-router.delete('/:id', bookingController.deleteBooking); // Xóa booking theo ID
+router.post('/', protect, bookingController.createBooking);
+
+router.get('/', protect, bookingController.getBookings);
+
+router.get('/:id', protect, bookingController.getBookingById);
+
+router.put('/:id', protect, bookingController.updateBooking);
+
+router.delete('/:id', protect, adminOnly, bookingController.deleteBooking);
 
 export default router;
